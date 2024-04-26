65e45c883e8e2f6b9d64714581daaf3c5d3cf41f
JayaSanthosh7066 <jayasanthoshamalakanti@gmail.com>
2024-04-26 08:09:24 +0530

    2nd part ui done

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
-import sendMessage from "../controllers/message.controller.js";
-import getMessage from "../controllers/message.controller.js";
+import { sendMessage } from "../controllers/message.controller.js";
+import { getMessage } from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 const router = express.Router();
 
---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,15 @@
+import Conversation from "./Conversation";
+
+const Conversations = () => {
+  return (
+    <div className="py-2 flex flex-col">
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+    </div>
+  );
+};
+export default Conversations;
