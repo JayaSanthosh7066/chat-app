2c94a51d2fe3fcc20f98aec982530b57b946cc61
JayaSanthosh7066 <jayasanthoshamalakanti@gmail.com>
2024-04-28 23:56:32 +0530

    deplayment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
@@ -7,14 +8,17 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { app, server } from "./socket/socket.js";
 app.use(cookieParser());
-
+const __dirname = path.resolve();
 dotenv.config();
 const PORT = process.env.PORT || 6000;
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen(PORT, () => {
   connect();
   console.log("server listening at", PORT);
