841a46751628924889a881258d0040516fc310d3
JayaSanthosh7066 <jayasanthoshamalakanti@gmail.com>
2024-04-29 13:52:07 +0530

    page_open_with_login

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -8,7 +8,7 @@ export const useAuthContext = () => {
 
 export const AuthContextProvider = ({ children }) => {
   const [authUser, setAuthUser] = useState(
-    JSON.parse(localStorage.getItem("chat-user")) || "User"
+    JSON.parse(localStorage.getItem("chat-user")) || ""
   );
 
   return (
