06001532e9082d00d56476a91ab1e43aa7f8cf6d
JayaSanthosh7066 <jayasanthoshamalakanti@gmail.com>
2024-04-29 09:01:48 +0530

    final_deploy

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -12,7 +12,7 @@ export const SocketContextProvider = ({ children }) => {
   const { authUser } = useAuthContext();
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chat-app-1-7p2l.onrender.com", {
         query: {
           userId: authUser._id,
         },
